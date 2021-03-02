331a3da154bccc6605ecb0154dedb6bb3dc1f0a0
Caleb Smith <karagenit@outlook.com>
2021-03-02 12:10:11 -0500

    Implement Sip.sh

--- substrate-node/scripts/util/sip.sh
+++ substrate-node/scripts/util/sip.sh
@@ -7,7 +7,7 @@
 
 domain=$1
 ip=$(dig $domain +short | head)
+pubkey=$(echo | openssl s_client -connect $domain:443 2>/dev/null | openssl x509 -pubkey -noout | head -n -1 | tail -n +2 | tr -d '\n')
 
-#echo | openssl s_client -showcerts -servername gnupg.org -connect gnupg.org:443 2>/dev/null | openssl x509 -inform pem -noout -text
-
-echo | openssl s_client -connect $domain:443 | openssl x509 -pubkey -noout
+echo "Adding $domain ($ip) (${pubkey:0:16}...)"
+./feed.sh $domain $ip $pubkey
