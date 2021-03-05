03564e0af4f05d0cb4617cc8f2e8187bf4060119
Caleb Smith <karagenit@outlook.com>
2021-03-05 13:36:54 -0500

    Make Sip.sh Print Public Key in Hex

--- substrate-node/scripts/util/sip.sh
+++ substrate-node/scripts/util/sip.sh
@@ -7,7 +7,7 @@
 
 domain=$1
 ip=$(dig $domain +short | head -n1)
-pubkey=$(echo | openssl s_client -connect $domain:443 2>/dev/null | openssl x509 -pubkey -noout | head -n -1 | tail -n +2 | tr -d '\n')
+pubkey=$(echo | openssl s_client -connect $domain:443 2>/dev/null | openssl x509 -pubkey -noout | head -n -1 | tail -n +2 | tr -d '\n' | base64 -d | od -t x1 -An | tr -d ' ' | tr -d '\n')
 
 echo "$domain $ip $pubkey"
 ./add.sh $domain $ip $pubkey
