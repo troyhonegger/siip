79f60b19ce0790238ba221bdb19d355a2e3ce907
Caleb Smith <karagenit@outlook.com>
2021-03-05 10:47:33 -0500

    Handle Return Value

--- substrate-node/node/src/rpc.rs
+++ substrate-node/node/src/rpc.rs
@@ -40,17 +40,24 @@ impl<C> SiipRpcStruct<C> {
 
 impl<C> SiipRpcTrait for SiipRpcStruct<C> where C: Send + Sync + 'static {
     fn add_cert(&self, domain: String, ip: String, pubkey: String) -> SystemResult<String> {
-        SiipModule::<Runtime>::register_certificate(
+        let res = SiipModule::<Runtime>::register_certificate(
             siip_node_runtime::Origin::signed(
                 crate::chain_spec::get_account_id_from_seed::<sr25519::Public>("Alice")
             ),
             "Caleb".as_bytes().to_vec(),
-            domain.as_bytes().to_vec(),
+            //domain.as_bytes().to_vec(),
+            "test.org".as_bytes().to_vec(),
             ip.as_bytes().to_vec(),
             "Info".as_bytes().to_vec(),
             pubkey.as_bytes().to_vec()
         );
-        Ok(domain)
+
+        let msg = match res {
+            Ok(()) => domain,
+            Err(dispatch) => "uh oh".to_string(),
+        };
+
+        Ok(msg)
     }
 }
 
