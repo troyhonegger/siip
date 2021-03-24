ab2478a4feed8dfc52e2f51325e39c1f2fc522bd
Adrian Teigen <ateigen@protonmail.com>
2021-03-24 11:01:02 -0400

    Works so far.

--- substrate-ui/src/Interactor.js
+++ substrate-ui/src/Interactor.js
@@ -12,9 +12,10 @@ function Main (props) {
   const { accountPair } = props;
   const [status, setStatus] = useState(null);
 
-  const [interxType, setInterxType] = useState('EXTRINSIC');
-  const [palletRPCs, setPalletRPCs] = useState([]);
-  const [callables, setCallables] = useState([]);
+  // const [interxType, setInterxType] = useState('EXTRINSIC');
+  const [interxType] = useState('EXTRINSIC');
+  // const [palletRPCs, setPalletRPCs] = useState([]);
+  // const [callables, setCallables] = useState([]);
   const [paramFields, setParamFields] = useState([]);
 
   const initFormState = {
@@ -23,36 +24,49 @@ function Main (props) {
     inputParams: []
   };
 
+  // api: [object Object]
+  // palletRpc: siipModule
+  // callable: registerCertificate
+
   const [formState, setFormState] = useState(initFormState);
   const { palletRpc, callable, inputParams } = formState;
 
-  const getApiType = (api, interxType) => {
-    if (interxType === 'QUERY') {
-      return api.query;
-    } else if (interxType === 'EXTRINSIC') {
-      return api.tx;
-    } else if (interxType === 'RPC') {
-      return api.rpc;
-    } else {
-      return api.consts;
-    }
-  };
+  const getApiType = (api, interxType) => api.tx;
+  // const getApiType = (api, interxType) => {
+  //   if (interxType === 'QUERY') {
+  //     return api.query;
+  //   } else if (interxType === 'EXTRINSIC') {
+  //     return api.tx;
+  //   } else if (interxType === 'RPC') {
+  //     return api.rpc;
+  //   } else {
+  //     return api.consts;
+  //   }
+  // };
 
-  const updatePalletRPCs = () => {
-    if (!api) { return; }
-    const apiType = getApiType(api, interxType);
-    const palletRPCs = Object.keys(apiType).sort()
-      .filter(pr => Object.keys(apiType[pr]).length > 0)
-      .map(pr => ({ key: pr, value: pr, text: pr }));
-    setPalletRPCs(palletRPCs);
-  };
+  const siipModule = {key: "siipModule", value: "siipModule", text: "siipModule" };
+  // const updatePalletRPCs = () => setPalletRPCs([siipModule]);
+  const palletRPCs = [siipModule];
+  // const updatePalletRPCs = () => {
+  //   if (!api) { return; }
+  //   const apiType = getApiType(api, interxType);
+  //   const palletRPCs = Object.keys(apiType).sort()
+  //     .filter(pr => Object.keys(apiType[pr]).length > 0)
+  //     .map(pr => ({ key: pr, value: pr, text: pr }));
+  //   setPalletRPCs(palletRPCs);
+  // };
 
-  const updateCallables = () => {
-    if (!api || palletRpc === '') { return; }
-    const callables = Object.keys(getApiType(api, interxType)[palletRpc]).sort()
-      .map(c => ({ key: c, value: c, text: c }));
-    setCallables(callables);
-  };
+
+  const registerCertificate = { key: "registerCertificate", value: "registerCertificate", text: "registerCertificate" };
+  // const updateCallables = () => setCallables([registerCertificate]);
+  const callables = [registerCertificate];
+  // const updateCallables = () => {
+  //   if (!api || palletRpc === '') { return; }
+  //   const callables = Object.keys(getApiType(api, interxType)[palletRpc]).sort()
+  //     .map(c => ({ key: c, value: c, text: c }));
+  //
+  //   setCallables(callables);
+  // };
 
   const updateParamFields = () => {
     if (!api || palletRpc === '' || callable === '') {
@@ -62,60 +76,73 @@ function Main (props) {
 
     let paramFields = [];
 
-    if (interxType === 'QUERY') {
-      const metaType = api.query[palletRpc][callable].meta.type;
-      if (metaType.isPlain) {
-        // Do nothing as `paramFields` is already set to []
-      } else if (metaType.isMap) {
-        paramFields = [{
-          name: metaType.asMap.key.toString(),
-          type: metaType.asMap.key.toString(),
-          optional: false
-        }];
-      } else if (metaType.isDoubleMap) {
-        paramFields = [{
-          name: metaType.asDoubleMap.key1.toString(),
-          type: metaType.asDoubleMap.key1.toString(),
-          optional: false
-        }, {
-          name: metaType.asDoubleMap.key2.toString(),
-          type: metaType.asDoubleMap.key2.toString(),
-          optional: false
-        }];
-      }
-    } else if (interxType === 'EXTRINSIC') {
-      const metaArgs = api.tx[palletRpc][callable].meta.args;
+    const metaArgs = api.tx[palletRpc][callable].meta.args;
 
-      if (metaArgs && metaArgs.length > 0) {
-        paramFields = metaArgs.map(arg => ({
-          name: arg.name.toString(),
-          type: arg.type.toString(),
-          optional: argIsOptional(arg)
-        }));
-      }
-    } else if (interxType === 'RPC') {
-      let metaParam = [];
-
-      if (jsonrpc[palletRpc] && jsonrpc[palletRpc][callable]) {
-        metaParam = jsonrpc[palletRpc][callable].params;
-      }
-
-      if (metaParam.length > 0) {
-        paramFields = metaParam.map(arg => ({
-          name: arg.name,
-          type: arg.type,
-          optional: arg.isOptional || false
-        }));
-      }
-    } else if (interxType === 'CONSTANT') {
-      paramFields = [];
+    if (metaArgs && metaArgs.length > 0) {
+      paramFields = metaArgs.map(arg => ({
+        name: arg.name.toString(),
+        type: arg.type.toString(),
+        optional: argIsOptional(arg)
+      }));
     }
+
+    //
+    // if (interxType === 'QUERY') {
+    //   const metaType = api.query[palletRpc][callable].meta.type;
+    //   if (metaType.isPlain) {
+    //     // Do nothing as `paramFields` is already set to []
+    //   } else if (metaType.isMap) {
+    //     paramFields = [{
+    //       name: metaType.asMap.key.toString(),
+    //       type: metaType.asMap.key.toString(),
+    //       optional: false
+    //     }];
+    //   } else if (metaType.isDoubleMap) {
+    //     paramFields = [{
+    //       name: metaType.asDoubleMap.key1.toString(),
+    //       type: metaType.asDoubleMap.key1.toString(),
+    //       optional: false
+    //     }, {
+    //       name: metaType.asDoubleMap.key2.toString(),
+    //       type: metaType.asDoubleMap.key2.toString(),
+    //       optional: false
+    //     }];
+    //   }
+    // } else if (interxType === 'EXTRINSIC') {
+    //   const metaArgs = api.tx[palletRpc][callable].meta.args;
+    //
+    //   if (metaArgs && metaArgs.length > 0) {
+    //     paramFields = metaArgs.map(arg => ({
+    //       name: arg.name.toString(),
+    //       type: arg.type.toString(),
+    //       optional: argIsOptional(arg)
+    //     }));
+    //   }
+    // } else if (interxType === 'RPC') {
+    //   let metaParam = [];
+    //
+    //   if (jsonrpc[palletRpc] && jsonrpc[palletRpc][callable]) {
+    //     metaParam = jsonrpc[palletRpc][callable].params;
+    //   }
+    //
+    //   if (metaParam.length > 0) {
+    //     paramFields = metaParam.map(arg => ({
+    //       name: arg.name,
+    //       type: arg.type,
+    //       optional: arg.isOptional || false
+    //     }));
+    //   }
+    // } else if (interxType === 'CONSTANT') {
+    //   paramFields = [];
+    // }
 
     setParamFields(paramFields);
   };
 
-  useEffect(updatePalletRPCs, [api, interxType]);
-  useEffect(updateCallables, [api, interxType, palletRpc]);
+  // useEffect(updatePalletRPCs, []);
+  // useEffect(updatePalletRPCs, [api, interxType]);
+  // useEffect(updateCallables, []);
+  // useEffect(updateCallables, [api, interxType, palletRpc]);
   useEffect(updateParamFields, [api, interxType, palletRpc, callable, jsonrpc]);
 
   const onPalletCallableParamChange = (_, data) => {
@@ -137,11 +164,11 @@ function Main (props) {
     });
   };
 
-  const onInterxTypeChange = (ev, data) => {
-    setInterxType(data.value);
-    // clear the formState
-    setFormState(initFormState);
-  };
+  // const onInterxTypeChange = (ev, data) => {
+  //   setInterxType(data.value);
+  //   // clear the formState
+  //   setFormState(initFormState);
+  // };
 
   const getOptionalMsg = (interxType) =>
     interxType === 'RPC'
@@ -152,37 +179,37 @@ function Main (props) {
     <Grid.Column width={8}>
       <h1>Pallet Interactor</h1>
       <Form>
-        <Form.Group style={{ overflowX: 'auto' }} inline>
-          <label>Interaction Type</label>
-          <Form.Radio
-            label='Extrinsic'
-            name='interxType'
-            value='EXTRINSIC'
-            checked={interxType === 'EXTRINSIC'}
-            onChange={onInterxTypeChange}
-          />
-          <Form.Radio
-            label='Query'
-            name='interxType'
-            value='QUERY'
-            checked={interxType === 'QUERY'}
-            onChange={onInterxTypeChange}
-          />
-          <Form.Radio
-            label='RPC'
-            name='interxType'
-            value='RPC'
-            checked={interxType === 'RPC'}
-            onChange={onInterxTypeChange}
-          />
-          <Form.Radio
-            label='Constant'
-            name='interxType'
-            value='CONSTANT'
-            checked={interxType === 'CONSTANT'}
-            onChange={onInterxTypeChange}
-          />
-        </Form.Group>
+        {/*<Form.Group style={{ overflowX: 'auto' }} inline>*/}
+        {/*  <label>Interaction Type</label>*/}
+        {/*  <Form.Radio*/}
+        {/*    label='Extrinsic'*/}
+        {/*    name='interxType'*/}
+        {/*    value='EXTRINSIC'*/}
+        {/*    checked={interxType === 'EXTRINSIC'}*/}
+        {/*    onChange={onInterxTypeChange}*/}
+        {/*  />*/}
+        {/*  <Form.Radio*/}
+        {/*    label='Query'*/}
+        {/*    name='interxType'*/}
+        {/*    value='QUERY'*/}
+        {/*    checked={interxType === 'QUERY'}*/}
+        {/*    onChange={onInterxTypeChange}*/}
+        {/*  />*/}
+        {/*  <Form.Radio*/}
+        {/*    label='RPC'*/}
+        {/*    name='interxType'*/}
+        {/*    value='RPC'*/}
+        {/*    checked={interxType === 'RPC'}*/}
+        {/*    onChange={onInterxTypeChange}*/}
+        {/*  />*/}
+        {/*  <Form.Radio*/}
+        {/*    label='Constant'*/}
+        {/*    name='interxType'*/}
+        {/*    value='CONSTANT'*/}
+        {/*    checked={interxType === 'CONSTANT'}*/}
+        {/*    onChange={onInterxTypeChange}*/}
+        {/*  />*/}
+        {/*</Form.Group>*/}
         <Form.Field>
           <Dropdown
             placeholder='Pallets / RPC'
@@ -245,24 +272,27 @@ function Main (props) {
 }
 
 function InteractorSubmit (props) {
-  const { attrs: { interxType } } = props;
-  if (interxType === 'QUERY') {
-    return <TxButton
-      label = 'Query'
-      type = 'QUERY'
-      color = 'blue'
-      {...props}
-    />;
-  } else if (interxType === 'EXTRINSIC') {
-    return <TxGroupButton {...props} />;
-  } else if (interxType === 'RPC' || interxType === 'CONSTANT') {
-    return <TxButton
-      label = 'Submit'
-      type = {interxType}
-      color = 'blue'
-      {...props}
-    />;
-  }
+  return <TxGroupButton {...props} />;
+
+  // const { attrs: { interxType } } = props;
+  // if (interxType === 'QUERY') {
+  //   return <TxButton
+  //     label = 'Query'
+  //     type = 'QUERY'
+  //     color = 'blue'
+  //     {...props}
+  //   />;
+  // } else if (interxType === 'EXTRINSIC') {
+  //   return <TxGroupButton {...props} />;
+  // } else if (interxType === 'RPC' || interxType === 'CONSTANT') {
+  //   return <TxButton
+  //     label = 'Submit'
+  //     type = {interxType}
+  //     color = 'blue'
+  //     {...props}
+  //   />;
+  // }
+  //
 }
 
 export default function Interactor (props) {
