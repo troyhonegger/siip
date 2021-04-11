eb607b58b1b293850e4eff83eaa1a5204ea97274
Sam Bove <boves@purdue.edu>
2021-04-11 15:50:00 -0400

    added react router navbar

---
+++ substrate-ui/src/routes.js
@@ -0,0 +1,6 @@
+export const BASE_PATH = '/substrate-front-end-template';
+export const SIIP_PATH = `${BASE_PATH}/siip`;
+export const DEMOCRACY_PATH = `${BASE_PATH}/democracy`;
+export const METRICS_PATH = `${BASE_PATH}/metrics`;
+export const BALANCES_PATH = `${BASE_PATH}/balances`;
+export const INTERACTOR_PATH = `${BASE_PATH}/interactor`;
