60ecf9fb836033a012fd8f19a354b5d383e0ca38
Sam Bove <boves@purdue.edu>
2021-04-19 15:52:16 -0400

    sip.sh formats public keys with colons and capital hex

--- client/sip.sh
+++ client/sip.sh
@@ -7,7 +7,7 @@
 
 domain=$1
 ip=$(dig $domain +short | head -n1)
-pubkey=$(echo | openssl s_client -connect $domain:443 2>/dev/null | openssl x509 -pubkey -noout | head -n -1 | tail -n +2 | tr -d '\n' | base64 -d | od -t x1 -An | tr -d ' ' | tr -d '\n')
+pubkey=$(echo | openssl s_client -connect $domain:443 2>/dev/null | openssl x509 -pubkey -noout | head -n -1 | tail -n +2 | tr -d '\n' | base64 -d | od -t x1 -An | tr -d ' ' | tr -d '\n' | tr [:lower:] [:upper:] | sed 's/.\{2\}/&:/g;s/:$//' )
 
 echo "$domain $ip $pubkey"
 ./register.py $domain 'sip' $ip '{}' $pubkey
