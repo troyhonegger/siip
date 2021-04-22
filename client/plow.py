c5d5defcc871bdcb8d21ebe3bd36da2d027472ac
Caleb Smith <karagenit@outlook.com>
2021-04-22 14:48:18 -0400

    Let Plow Take IPs

--- client/plow.py
+++ client/plow.py
@@ -3,9 +3,9 @@
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("You must specify which domain's SIIP certificate to query for.")
-        print("Usage: plow.py DOMAIN")
+        print("Usage: plow.py DOMAIN [NODE IP(s)...]")
         sys.exit()
 
 # We moved these after the argument check so it fails faster :)
@@ -13,34 +13,47 @@ import substrateinterface
 from substrateinterface import SubstrateInterface, Keypair
 from substrateinterface.exceptions import SubstrateRequestException
 
-substrate = SubstrateInterface(
-    url="http://127.0.0.1:9933",
-    ss58_format=42,
-    type_registry_preset='polkadot', 
-    type_registry={
-        "types": {
-            "Certificate": {
-                "type": "struct",
-                "type_mapping": [
-                    ["version_number", "i32"],
-                    ["owner_id", "AccountId"],
-                    ["name", "Vec<u8>"],
-                    ["info", "Vec<u8>"],
-                    ["key", "Vec<u8>"],
-                    ["ip_addr", "Vec<u8>"],
-                    ["domain", "Vec<u8>"]
-                ]
+def plow(domain, node_urls=["http://127.0.0.1:9933"]):
+    print(node_urls)
+    # TODO: for each, plow_single each url, compare results
+
+def plow_single(domain, node_url):
+    # TODO: validate node_url is formatted correctly?
+    substrate = SubstrateInterface(
+        url=node_url,
+        ss58_format=42,
+        type_registry_preset='polkadot',
+        type_registry={
+            "types": {
+                "Certificate": {
+                    "type": "struct",
+                    "type_mapping": [
+                        ["version_number", "i32"],
+                        ["owner_id", "AccountId"],
+                        ["name", "Vec<u8>"],
+                        ["info", "Vec<u8>"],
+                        ["key", "Vec<u8>"],
+                        ["ip_addr", "Vec<u8>"],
+                        ["domain", "Vec<u8>"]
+                    ]
+                }
             }
         }
-    }
-)
-
-def plow(domain):
+    )
+    
     return substrate.query('SiipModule', 'CertificateMap', params=[domain])
 
 if __name__ == '__main__':
     domain = sys.argv[1]
-    siip = plow(domain)
+
+    # If IPs were specified on the command line, use those
+    if len(sys.argv) > 2:
+        siip = plow(domain, sys.argv[2:])
+    # TODO else if file /etc/siip exists, use those ips
+    # Otherwise use the built-in domains
+    else:
+        siip = plow(domain)
+
     print(siip)
 
     if siip is None:
