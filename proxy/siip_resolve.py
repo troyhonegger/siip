5c0316681b1b2a97cdef41d42ad0ec15befc606d
Caleb Smith <karagenit@outlook.com>
2021-04-27 11:05:06 -0400

    Resolve Fallback Public Key

--- proxy/siip_resolve.py
+++ proxy/siip_resolve.py
@@ -2,12 +2,14 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir, 'client'))
 from plow import plow
 import socket
+import ssl
+import base64
+import OpenSSL
 
 from siip_certificate import SiipCertificate
 
 def resolve(domain):
     cert_fields = plow(domain)
-    print(cert_fields)
     if cert_fields is None:
         return fallback_resolve(domain)
     cert_fields = cert_fields.value
@@ -18,10 +20,20 @@ def resolve(domain):
         cert_fields['key']
     )
 
-# TODO: resolve the domain name using standard DNS
 def fallback_resolve(domain):
+    ip = socket.gethostbyname(domain)
+
+    cert_pem = ssl.get_server_certificate((domain, 443))
+    cert_x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_pem)
+    pubkey = OpenSSL.crypto.dump_publickey(OpenSSL.crypto.FILETYPE_PEM, cert_x509.get_pubkey())
+    pubkey = pubkey.decode('utf-8')
+    pubkey = pubkey.replace('-----BEGIN PUBLIC KEY-----', '')
+    pubkey = pubkey.replace('-----END PUBLIC KEY-----', '')
+    pubkey = pubkey.replace('\n', '')
+    pubkey = base64.b64decode(pubkey).hex()
+
     return SiipCertificate(
-        socket.gethostbyname(domain),
+        ip,
         domain,
-        "12:34",
+        pubkey,
     )
