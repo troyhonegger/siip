e46d879ec5e32e8ee0be59a30af260a408bb01d4
Sam Bove <boves@purdue.edu>
2021-04-30 12:17:34 -0400

    bugfix

--- substrate-ui/src/substrate-lib/components/TxButton.js
+++ substrate-ui/src/substrate-lib/components/TxButton.js
@@ -100,7 +100,7 @@ function TxButton ({
       ? api.tx[palletRpc][callable](...transformed)
       : api.tx[palletRpc][callable]();
 
-    const unsub = await txExecute.signAndSend(fromAcct, { tip }, txResHandler)
+    const unsub = await txExecute.signAndSend(fromAcct, { tip: tip?.value }, txResHandler)
       .catch(txErrHandler);
     setUnsub(() => unsub);
   };
@@ -240,7 +240,8 @@ TxButton.propTypes = {
     callable: PropTypes.string,
     inputParams: PropTypes.array,
     paramFields: PropTypes.array
-  }).isRequired
+  }).isRequired,
+  tip: PropTypes.object
 };
 
 function TxGroupButton (props) {
